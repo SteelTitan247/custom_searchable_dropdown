536bebcf93843de1a07b2fad0d986c115c92c075
Shreyansh Sharma <shreyansh.sharma@orchids.edu.in>
2024-07-12 17:33:43 +0530

    Added some code in main activity kotlin file

--- android/app/src/main/kotlin/com/example/custom_in_place_searchable_dropdown/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/custom_in_place_searchable_dropdown/MainActivity.kt
@@ -2,5 +2,25 @@ package com.example.custom_in_place_searchable_dropdown
 
 import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity: FlutterActivity() {
+package custom_dropdown
+
+import io.flutter.embedding.engine.plugins.FlutterPlugin
+
+class CustomDropdownPlugin: FlutterPlugin {
+    override fun onAttachedToEngine(flutterPluginBinding: FlutterPlugin.FlutterPluginBinding) {
+        channel = MethodChannel(flutterPluginBinding.binaryMessenger, "custom_dropdown")
+        channel.setMethodCallHandler(this)
+    }
+
+    override fun onDetachedFromEngine(binding: FlutterPlugin.FlutterPluginBinding) {
+        channel.setMethodCallHandler(null)
+    }
+
+    override fun onMethodCall(call: MethodCall, result: Result) {
+        if (call.method == "getPlatformVersion") {
+            result.success("Android ${android.os.Build.VERSION.RELEASE}")
+        } else {
+            result.notImplemented()
+        }
+    }
 }
